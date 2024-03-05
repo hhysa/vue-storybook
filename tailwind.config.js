f30dd61ad9844971fb80e5f1e264cd734d86e26d
hane.hysa <hanehysa@2rmlab.net>
2024-03-05 10:23:39 +0100

    --fix: tailwind config warn

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend: {},
