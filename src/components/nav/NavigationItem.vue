a9bb5e40403595ffc48676ca1e6c9905c3f47b0f
hhysa <h.hysa02@gmail.com>
2024-08-15 18:50:59 +0200

    --fix: active link in nav bar

--- src/components/nav/NavigationItem.vue
+++ src/components/nav/NavigationItem.vue
@@ -6,35 +6,36 @@
     <ul
       class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0"
     >
-      <li>
-        <a
-          href="#"
-          class="block py-2 px-3 text-white bg-green-700 rounded md:bg-transparent md:text-green-700 md:p-0 md:dark:text-green-500"
-          aria-current="page"
-          >Home</a
+      <li v-for="link in links" :key="link.name">
+        <router-link
+          :to="link.path"
+          :class="[
+            'block py-2 px-3 rounded md:p-0',
+            isActive(link.path)
+              ? 'text-white bg-green-700 md:bg-transparent md:text-green-700 dark:text-green-500'
+              : 'text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-green-700 dark:hover:text-green-500 dark:text-white dark:hover:bg-gray-700 md:dark:hover:bg-transparent dark:border-gray-700',
+          ]"
         >
-      </li>
-      <li>
-        <a
-          href="#about"
-          class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-green-700 md:p-0 md:dark:hover:text-green-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-          >About</a
-        >
-      </li>
-      <li>
-        <a
-          href="#services"
-          class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-green-700 md:p-0 md:dark:hover:text-green-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-          >Services</a
-        >
-      </li>
-      <li>
-        <a
-          href="#portfolio"
-          class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-green-700 md:p-0 md:dark:hover:text-green-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-          >Portfolio</a
-        >
+          {{ link.name }}
+        </router-link>
       </li>
     </ul>
   </div>
 </template>
+
+<script setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const links = [
+  { name: 'Home', path: '/' },
+  { name: 'About', path: '/about' },
+  { name: 'Services', path: '/services' },
+  { name: 'Portfolio', path: '/portfolio' },
+];
+
+const isActive = (path) => {
+  return router.currentRoute.value.path === path;
+};
+</script>
